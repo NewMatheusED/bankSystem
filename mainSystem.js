80719fe9bd9715cb52c04c04001a8d38634f504a
Matheus <matheus081105@gmail.com>
2024-04-30 20:14:08 -0300

    :construction: Construção da base HTML do sistema

--- mainSystem.js
+++ mainSystem.js
@@ -97,14 +97,14 @@ class Conta {
     }
 }
 
-let sistema = new systemAccount();
-let conta1 = new Conta("Matheus", "123456", 1000);
-let conta2 = new Conta("Lucas", "654321", 500);
-sistema._addAccount(conta1);
-sistema._addAccount(conta2);
-conta1._deposit(200);
-conta1._deposit(500);
-conta1._withdraw(1000);
-conta1._transfer(500, "654321", sistema);
-conta1._getExtract();
+// let sistema = new systemAccount();
+// let conta1 = new Conta("Matheus", "123456", 1000);
+// let conta2 = new Conta("Lucas", "654321", 500);
+// sistema._addAccount(conta1);
+// sistema._addAccount(conta2);
+// conta1._deposit(200);
+// conta1._deposit(500);
+// conta1._withdraw(1000);
+// conta1._transfer(500, "654321", sistema);
+// conta1._getExtract();
 //let conta = new Conta(123, "123456", "1000"); error example
