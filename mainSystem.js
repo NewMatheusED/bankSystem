1a8aba98e16dc2d1e50aa8c5a6371ee4a243aa65
Matheus <matheus081105@gmail.com>
2024-04-30 20:18:26 -0300

    :sparkles: Adicionando função de visualição de todas as contas bancárias

--- mainSystem.js
+++ mainSystem.js
@@ -25,6 +25,10 @@ class systemAccount {
         console.log('Account doesnt exist');
         return false;
     }
+
+    _getAllAccounts() {
+        console.log(this.accounts);
+    }
     
     
 }
